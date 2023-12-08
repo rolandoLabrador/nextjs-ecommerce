a46d409ecaf138a96d20d4402149b3e86ca40d11
Roland Labrador <rolando1515@hotmail.com>
2023-12-08 12:28:56 -0500

    Navbar

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "./logo.png";
+import logo from "@/assets/logo.png";
 
 
 export default function Navbar() {
-  return (
-    <div className="bg-base-100">
-      <div className="max-w-7x navbar m-auto flex-col gap-2 sm:flex-row"></div>
-            <div className="flex-1">
-                <link href="/">
-                    <Image src={logo} height={40} width={40} alt="Flowmazon logo" />
-
-                </link>
-            </div>
-    </div>
-  );
-}
+    return (
+      <div className="bg-base-100">
+        <div className="max-w-7x navbar m-auto flex-col gap-2 sm:flex-row">
+          <div className="flex-1">
+          <Link href="/" className="btn-ghost btn text-xl normal-case">
+            <Image src={logo} height={40} width={40} alt="Flowmazon logo" />
+            Flowmazon
+          </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
