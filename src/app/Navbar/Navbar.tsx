2c404d72aa6fa313a04751edc834d61013d82140
Roland Labrador <rolando1515@hotmail.com>
2023-12-09 13:55:05 -0500

    commit

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -2,38 +2,42 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/logo.png";
 import { redirect } from "next/navigation";
+import { getCart } from "@/lib/db/cart";
+import ShoppingCartButton from "./ShoppingCartButton";
 
-async function searchProduct(formData: FormData) {
+async function searchProducts(formData: FormData) {
     "use server";
-
+  
     const searchQuery = formData.get("searchQuery")?.toString();
-
-    if(searchQuery) {
-        redirect ("/search?query="+searchQuery);
-
+  
+    if (searchQuery) {
+      redirect("/search?query=" + searchQuery);
     }
-}
-
-export default function Navbar() {
+  }
+  
+  export default async function Navbar() {
+    const cart = await getCart();
+  
     return (
       <div className="bg-base-100">
-        <div className="max-w-7x navbar m-auto flex-col gap-2 sm:flex-row">
+        <div className="navbar m-auto max-w-7xl flex-col gap-2 sm:flex-row">
           <div className="flex-1">
-          <Link href="/" className="btn-ghost btn text-xl normal-case">
-            <Image src={logo} height={40} width={40} alt="Flowmazon logo" />
-            Flowmazon
-          </Link>
+            <Link href="/" className="btn-ghost btn text-xl normal-case">
+              <Image src={logo} height={40} width={40} alt="Flowmazon logo" />
+              Flowmazon
+            </Link>
           </div>
           <div className="flex-none gap-2">
-            <form action={searchProduct}>
-                <div className="form-control">
-                    <input
-                        name="searchQuery"
-                        placeholder="Search"
-                        className="input input-bordered w-full min-w-[1000px]"
-                        />
-                    </div>
+            <form action={searchProducts}>
+              <div className="form-control">
+                <input
+                  name="searchQuery"
+                  placeholder="Search"
+                  className="input-bordered input w-full min-w-[100px]"
+                />
+              </div>
             </form>
+            <ShoppingCartButton cart={cart} />
           </div>
         </div>
       </div>
