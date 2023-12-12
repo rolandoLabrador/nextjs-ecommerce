b80051a9e1876188526f99de13cf9be7887438a2
Roland Labrador <rolando1515@hotmail.com>
2023-12-12 10:43:46 -0500

    db

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,21 @@
 import { prisma } from "@/lib/db/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { env } from "@/lib/env";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+
 export const authOptions: NextAuthOptions = {
-    adapter:PrismaAdapter(prisma)as Adapter,
-    providers:[
-        GoogleProvider({
-
-            clientId:process.env.GOOGLE_CLIENT_ID,
-            clientSecret:process.env.GOOGLE_CLIENT_SECRET
-        })
-
-        
-
-
-
-    ]
-
-
-}
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
 
 const handler = NextAuth(authOptions);
 
-export {handler as GET,handler as POST }
+export { handler as GET, handler as POST };
