22a9142d29d0dd056c4c991d047b89cbcafe42ef
Roland Labrador <rolando1515@hotmail.com>
2023-12-18 10:42:01 -0500

    commits

--- src/Data.ts
+++ src/Data.ts
@@ -8,37 +8,37 @@ interface Item {
 const data: Item[] = [
   {
     id: 1,
-    title1: "PRICE",
+    title1: "",
     title2: "No haggling. No pressure.",
     des: "We’ve eliminated the middleman and made the entire car buying process more efficient, which allows us to pass the savings on to our customers.",
   },
   {
     id: 2,
-    title1: "RECONDITIONING",
+    title1: "",
     title2: "Multiple Inspections",
     des: "Our cars undergo an extensive inspection and reconditioning process that must meet Vroom’s specific mechanical, electrical and cosmetic standards. Every car comes with a free vehicle history report.",
   },
   {
     id: 3,
-    title1: "FINANCE",
+    title1: "",
     title2: "Get a Competitive Rate",
     des: "Vroom has strategic relationships with partners like Chase, Santander, and Ally to secure the best financing for you.",
   },
   {
     id: 4,
-    title1: "DELIVER TO YOU",
+    title1: "",
     title2: "Delivery Straight to You",
     des: "We’ve eliminated the middleman and made the entire car buying process more efficient, which allows us to pass the savings on to our customers.",
   },
   {
     id: 5,
-    title1: "VEE-T",
+    title1: "",
     title2: "No haggling. No pressure.",
     des: "We’ve eliminated the middleman and made the entire car buying process more efficient, which allows us to pass the savings on to our customers.",
   },
   {
     id: 6,
-    title1: "TESTING",
+    title1: "",
     title2: "No haggling. No pressure.",
     des: "We’ve eliminated the middleman and made the entire car buying process more efficient, which allows us to pass the savings on to our customers.",
   },
