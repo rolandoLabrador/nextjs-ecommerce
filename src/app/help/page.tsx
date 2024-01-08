9d9c1ad266de24da664c967288fa4309fac1e263
Roland Labrador <rolando1515@hotmail.com>
2024-01-08 16:50:57 -0500

    commit

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 export default function Help() {
   return (
     <>
+   
+
     { /* This is the hero with an image */}
       <div className="mt-20 hero min-h-screen" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1498084393753-b411b2d26b34?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'}}>
   <div className="hero-overlay bg-opacity-60"></div>
