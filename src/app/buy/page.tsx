fc3c25738365fe77e995f2751a6897fab78145df
Roland Labrador <rolando1515@hotmail.com>
2024-01-14 14:53:22 -0500

    commit

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -1,18 +1,18 @@
-import SubNavbar from "@/components/subnavbar";
+import { useState } from "react";
 
-
-
-
-
+import Subnavbar from "@/components/subnavbar";
+import { prisma } from "@/lib/db/prisma";
 
 export default async function BuyProduct() {
-  return <>
- <SubNavbar />
-  
-  
-  
-  
-  
-  
-  </>;
+  const products = await prisma.product.findMany({
+    orderBy: { id: "desc" },
+  });
+
+  return (
+    <>
+      <Subnavbar products={products} />
+
+      {/* <Sidebar /> */}
+    </>
+  );
 }
