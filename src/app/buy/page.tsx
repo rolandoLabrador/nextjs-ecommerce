10d63ee8de6cfb6f4e8d595329334b698bc8c475
Roland Labrador <rolando1515@hotmail.com>
2024-01-13 20:40:59 -0500

    commit

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
-import Sidebar from "@/components/sidebar";
-import Subnavbar from "@/components/subnavbar";
-import { prisma } from "@/lib/db/prisma";
+import SubNavbar from "@/components/subnavbar";
+
+
+
+
+
 
 export default async function BuyProduct() {
-  const products = await prisma.product.findMany({
-    orderBy: { id: "desc" },
-  });
-
-  return (
-    <>
-      <Subnavbar products={products} />
-
-      {/* <Sidebar /> */}
-    </>
-  );
+  return <>
+ <SubNavbar />
+  
+  
+  
+  
+  
+  
+  </>;
 }
--- src/components/subnavbar.tsx
+++ src/components/subnavbar.tsx
@@ -1,68 +1,63 @@
 "use client";
-
 import React, { useState } from "react";
 
-type TProps = {
-  products: any[];
-};
+const SubNavbar=()=> {
+    {/*  hook it will hold all the item*/}
+    const [searchValue,setsearchValue] = useState(""); 
+    const [productArray,setProductArray] = useState([]); 
 
-const Subnavbar = (props: TProps) => {
-  const { products } = { ...props };
-  const [searchValue, setSearchValue] = useState("");
 
-  const productArray = products?.map((item) =>
-    item?.name?.toLowerCase().includes(searchValue.toLowerCase()) ? item : null
-  );
+
 
   return (
-    <>
-      <div className="navbar mt-20 bg-base-100">
-        <div className="flex-1">
-          <a className="btn-ghost btn text-xl">daisyUI</a>
-        </div>
-
-        <div className="flex-none gap-2">
-          <div className="form-control">
-            <input
-              type="text"
-              value={searchValue}
-              onChange={(event) => setSearchValue(event.target.value)}
-              placeholder="Search"
-              className="input-bordered input w-24 md:w-auto"
-              name="searchQuery"
-            />
-          </div>
-
-          {/* Rest of your code */}
-        </div>
+    <>  
+    <div className="navbar mt-20 bg-base-100">
+    <div className="flex-1">
+      <a className="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <div className="flex-none gap-2">
+      <div className="form-control">
+        <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
       </div>
-
-      <br></br>
-
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {productArray.map((item: any, index: number) => {
-          if (!item) return null;
-
-          return (
-            <div key={index} className="card bg-base-100 shadow-xl">
-              <figure className="px-10 pt-10">
-                <img src={item.imageUrl} alt="Shoes" className="rounded-xl" />
-              </figure>
-
-              <div className="card-body items-center text-center">
-                <h2 className="card-title">{item?.name}</h2>
-                <p>{item?.description}</p>
-                <h6>{`$${item?.price}`}</h6>
-                <div className="card-actions">
-                  <button className="btn-primary btn">Buy Now</button>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+      
       </div>
-    </>
+    </div>
+    <br></br>
+    {/*  grid  */}
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+
+
+    {/*  cards */}
+    <div className="card  bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h6>$20.00</h6>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+{/*  end of the grid  */}
+</div>
+
+</>
+
   );
-};
+}
 
-export default Subnavbar;
+
+export default SubNavbar;
