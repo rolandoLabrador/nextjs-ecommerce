535619ee4f578d4a115991ae965e993901bc7ce7
Roland Labrador <rolando1515@hotmail.com>
2024-02-12 09:27:58 -0500

    cypress

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,13 +1,13 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
-
-
-
-
-
-
-
+    cy.get('nav').contains('BUY').click();
+    cy.get('nav').contains('SELL').click();
+    cy.get('nav').contains('FINANCE').click();
+    cy.get('nav').contains('ABOUT').click();
+    cy.get('nav').contains('HELP').click();
+    cy.get('.indicator').click();
+    cy.get('.card-actions').click();
     
   })
 })
