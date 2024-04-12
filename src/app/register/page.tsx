c9bfc82552771044dc7867da103f4c9e4a12c4af
Roland Labrador <rolando1515@hotmail.com>
2024-04-11 20:26:29 -0400

    register page updated

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,10 +5,9 @@ import React, { useState } from "react";
 export default function Page() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
- async function handleSubmit() {
-  console.log("data",{email,password});
-
- }
+  async function handleSubmit() {
+    console.log("data", { email, password });
+  }
 
   return (
     // this is the form
@@ -36,7 +35,7 @@ export default function Page() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" >
+          <div className="space-y-6">
             <div>
               <label className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -47,7 +46,7 @@ export default function Page() {
                   name="email"
                   type="email"
                   value={email}
-                  onChange={(event)=>setEmail(event.target.value)}
+                  onChange={(event) => setEmail(event.target.value)}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -74,7 +73,7 @@ export default function Page() {
                   name="password"
                   type="password"
                   value={password}
-                  onChange={(event)=>setPassword(event.target.value)}
+                  onChange={(event) => setPassword(event.target.value)}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -83,13 +82,14 @@ export default function Page() {
 
             <div>
               <button
-                onSubmit={handleSubmit}
+                onClick={handleSubmit}
+                
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
               </button>
             </div>
-          </form>
+          </div>
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?
@@ -104,7 +104,7 @@ export default function Page() {
       </div>
     </div>
   );
-//}
-};
+  //}
+}
 
-// MIN 54
+// MIN 54:33
